3cc68cc621986e90088117bd84a1a6f2129f7515
Sanjith2003 <69067306+Sanjith2003@users.noreply.github.com>
2024-03-01 14:02:11 +0530

    Add files via upload

---
+++ test_code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct complex
+{
+    int real;
+    int imag;
+};
+
+func1()
+{
+    int number = 10;
+    float cgpa = 8.5;
+    struct complex obj;
+}
+
+func2(int a,int b)
+{
+    int value = a+b;
+    double height;
+}
+
+int main()
+{
+    int meo = 10;
+    func1();
+    func2(5, 10);
+}
